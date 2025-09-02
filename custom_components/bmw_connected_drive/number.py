9b88db36c13dca6a18488034ef0f0647c92178b7
Jyri Saukkonen <jyri.saukkonen+jjyksi@gmail.com>
2025-09-02 20:50:31 +0300

    ...

--- custom_components/bmw_connected_drive/number.py
+++ custom_components/bmw_connected_drive/number.py
@@ -16,7 +16,7 @@ from homeassistant.components.number import (
 )
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import HomeAssistantError
-from homeassistant.helpers.entity_platform import AddConfigEntryEntitiesCallback
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from . import DOMAIN, BMWConfigEntry
 from .coordinator import BMWDataUpdateCoordinator
@@ -58,7 +58,7 @@ NUMBER_TYPES: list[BMWNumberEntityDescription] = [
 async def async_setup_entry(
     hass: HomeAssistant,
     config_entry: BMWConfigEntry,
-    async_add_entities: AddConfigEntryEntitiesCallback,
+    async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up the MyBMW number from config entry."""
     coordinator = config_entry.runtime_data
